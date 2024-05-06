8df7345eb1f5a51e5940240b5dabf4c9817edf11
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-05-06 15:41:42 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React,{ReactNode} from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const RootLayout = ({children}:Props) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default RootLayout
